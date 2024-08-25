68e668966597eb38a8c1e91d524e7fa2cc638377
bielwdev <bielwdev@gmail.com>
2024-08-25 20:02:36 -0300

    docs(relational-operators): add examples of relational operators in Ruby

---
+++ module-02/relationaloperators.rb
@@ -0,0 +1,10 @@
+x = 10
+y = 20
+
+puts 'x = 10 && y = 20'
+puts "x == y: #{x == y}"   # false
+puts "x != y: #{x != y}"   # true
+puts "x > y: #{x > y}"     # false
+puts "x < y: #{x < y}"     # true
+puts "x >= y: #{x >= y}"   # false
+puts "x <= y: #{x <= y}"   # true
