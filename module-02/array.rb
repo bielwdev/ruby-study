3ae3b096864b5b7622603d6c169c7384cd98596c
bielwdev <bielwdev@gmail.com>
2024-08-27 17:14:46 -0300

    docs(array): add examples and explanations of array manipulation in Ruby

---
+++ module-02/array.rb
@@ -0,0 +1,50 @@
+# In Ruby, an array is a data structure that holds an ordered collection of 
+# objects. Arrays can store any type of object, including numbers, strings, and 
+# other arrays. The elements of an array are indexed, starting with 0 for the 
+# first element.
+
+# Creating an array
+fruits = ["apple", "banana", "cherry"]
+cars = Array.new
+
+# Accessing elements
+puts fruits[0]  # Output: "apple"
+puts fruits[1]  # Output: "banana"
+puts cars[0] # Output: null
+
+# Adding elements
+fruits << "date"
+cars << "bmw"
+cars.push("ferrari")
+puts fruits.inspect  # Output: ["apple", "banana", "cherry", "date"]
+puts cars.inspect # Output: ["bmw", "ferrari"]
+
+# Modifying elements
+fruits[1] = "blueberry"
+cars[1] = "buggati"
+fruits.delete("date")
+puts fruits.inspect  # Output: ["apple", "blueberry", "cherry"]
+puts cars.inspect # Output ["bmw", "bugatti"]
+
+# Iterating over an array
+fruits.each do |fruit|
+  puts fruit
+end
+
+cars.each do |carsbrand|
+  puts carsbrand
+end
+
+# Output:
+# "apple"
+# "blueberry"
+# "cherry"
+# "bmw"
+# "bugatti"
+
+
+### Key Points:
+# - Indexing: Elements are accessed by their index, starting at 0.
+# - Dynamic: Arrays can grow or shrink in size dynamically.
+# - Versatile: Arrays can store objects of different types within the 
+# same array.
