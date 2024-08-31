ad3a9667e91ac99f667c3cb158c94cb231c983a0
bielwdev <bielwdev@gmail.com>
2024-08-31 00:02:50 -0300

    docs(variable-interpolation): add example and explanation of variable interpolation in Ruby

---
+++ module-02/variables-interpolation.rb
@@ -0,0 +1,17 @@
+# In Ruby, variable interpolation allows you to insert the value of a variable 
+# into a string. This is done using `#{}` within double-quoted strings.
+
+### Example:
+
+name = "Gabriel"
+age = 22
+
+puts "My name is #{name} and I am #{age} years old."
+
+
+# Output
+# My name is Gabriel and I am 22 years old.
+
+#In this example, `#{name}` is replaced by the value of the `name` 
+# variable, and `#{age}` is replaced by the value of the `age` variable. 
+# This only works with double-quoted strings.
