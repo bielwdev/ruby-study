83bad96daf254b78e40ba21b04c25ee01ac8cb8e
bielwdev <bielwdev@gmail.com>
2024-09-01 13:28:44 -0300

    docs(print): explain the difference between print and puts in Ruby

---
+++ module-02/print.rb
@@ -0,0 +1,19 @@
+# In Ruby, `puts` and `print` are both used to display output to the console,
+# but they have some key differences.
+
+# `puts` adds a new line after the output
+puts "Hello, world!"
+puts "This is Ruby"
+
+# Output:
+# Hello, world!
+# This is Ruby
+# Notice that each string is printed on a new line.
+
+# `print` does not add a new line after the output
+print "Hello, world!"
+print "This is Ruby"
+
+# Output:
+# Hello, world!This is Ruby
+# Notice that the strings are printed on the same line.
